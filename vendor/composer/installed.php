e8c9e91b740a0d64b699ad60eda7e42dbfcfc79d
Alya Fedotova <fff.alya78@gmail.com>
2025-04-08 09:33:45 +0700

    composer commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fffal/server-side-practice',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '631606130aea198f543e2e3a686a53ffc7c2edb1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fffal/server-side-practice' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '631606130aea198f543e2e3a686a53ffc7c2edb1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
